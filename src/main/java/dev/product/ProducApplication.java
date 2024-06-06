d1068ecc3da570f73adeca00bf86b1148ccb3cad
Anaïs Kominiarz <kominiarz.anais@gmail.com>
2024-06-06 18:52:06 +0200

    feat(): demarrage application product

---
+++ src/main/java/dev/product/ProducApplication.java
@@ -0,0 +1,13 @@
+package dev.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/product/ProducApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
